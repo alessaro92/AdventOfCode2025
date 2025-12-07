61dc0d021813e71402cb5cb78939793d0db8f2ec
alessaro92 <alerex92@hotmail.it>
2025-12-07 18:58:51 +0100

    day 2 part 2 + stopwatch

--- AdventOfCode2025/Day02.cs
+++ AdventOfCode2025/Day02.cs
@@ -30,4 +30,50 @@ public static class Day02
 
         return sum;
     }
+
+    public static long Part2(string inputFilePath)
+    {
+        string content = File.ReadAllText(inputFilePath);
+        string[] ranges = content.Split(',');
+
+        long sum = 0;
+
+        foreach (string range in ranges)
+        {
+            string[] rangeNumbers = range.Split('-');
+            long rangeStart = long.Parse(rangeNumbers[0]);
+            long rangeEnd = long.Parse(rangeNumbers[1]);
+
+            // Same, but check if the string can be subdivided evenly for each possible substring size from its length to 2
+            for (long i = rangeStart; i <= rangeEnd; i++)
+            {
+                string currentNumberStr = i.ToString();
+                int currentNumberLength = currentNumberStr.Length;
+                for (int j = currentNumberLength; j >= 2; j--)
+                {
+                    if (currentNumberStr.Length % j != 0) continue;
+
+                    string[] chunks = currentNumberStr.Chunk(currentNumberStr.Length / j).Select(s => new string(s)).ToArray();
+
+                    bool equal = true;
+                    for (int k = 0; k < chunks.Length - 1; k++)
+                    {
+                        if (chunks[k] != chunks[k + 1])
+                        {
+                            equal = false;
+                            break;
+                        }
+                    }
+
+                    if (equal)
+                    {
+                        sum += i;
+                        break;
+                    }
+                }
+            }
+        }
+
+        return sum;
+    }
 }
--- AdventOfCode2025/Program.cs
+++ AdventOfCode2025/Program.cs
@@ -1,8 +1,16 @@
-﻿using AdventOfCode2025;
+﻿using System.Diagnostics;
+using AdventOfCode2025;
 
 Console.WriteLine("Input file path:");
 string inputFilePath = Console.ReadLine();
 
-var output = Day02.Part1(inputFilePath);
+var stopWatch = new Stopwatch();
+stopWatch.Start();
+
+var output = Day02.Part2(inputFilePath);
+
+stopWatch.Stop();
 
 Console.WriteLine(output);
+
+Console.WriteLine("Time: " + stopWatch.ElapsedMilliseconds);
